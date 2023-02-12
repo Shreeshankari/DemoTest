03324ced0e57aa60a03f90428fe6fb8d0c206f85
Shreeshankari <shankarishree96@gmail.com>
2023-02-12 21:53:44 +0530

    updates from person a

--- src/javaPractise/Methods.java
+++ src/javaPractise/Methods.java
@@ -5,6 +5,9 @@ public class Methods {
 	 //code
 	 System.out.println("header links validated");
 	 System.out.println("header links validated check git demotest");
+	 System.out.println("push from personA updates");
+	 System.out.println("push from personA updates1");
+	 System.out.println("push from personA updates2");
 	 return "Pass";
  }
 	
--- src/javaPractise/Parent.java
+++ src/javaPractise/Parent.java
@@ -5,6 +5,9 @@ public class Parent {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Navigated to Home Page");
+System.out.println("Page1");
+System.out.println("Page2");
+System.out.println("Page3");
 
 
 
