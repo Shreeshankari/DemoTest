684650bd4f9059645ea8397e3dfba408d91f95bf
Shreeshankari <shankarishree96@gmail.com>
2023-02-12 20:53:23 +0530

    GOOD START

---
+++ src/javaPractise/Methods.java
@@ -0,0 +1,10 @@
+package javaPractise;
+
+public class Methods {
+ public String validateHeader() {
+	 //code
+	 System.out.println("header links validated");
+	 return "Pass";
+ }
+	
+}
---
+++ src/javaPractise/Parent.java
@@ -0,0 +1,17 @@
+package javaPractise;
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Navigated to Home Page");
+
+
+
+Methods m = new Methods();
+//methodsobject.methodname
+System.out.println(m.validateHeader());
+
+	}
+
+}
