181e238fb8acd55e8a9d664291b1747be4d37346
Shreeshankari <shankarishree96@gmail.com>
2023-02-12 21:35:12 +0530

    this is commit from another person

--- src/javaPractise/Methods.java
+++ src/javaPractise/Methods.java
@@ -4,6 +4,7 @@ public class Methods {
  public String validateHeader() {
 	 //code
 	 System.out.println("header links validated");
+	 System.out.println("header links validated check git demotest");
 	 return "Pass";
  }
 	
